723c972f7c3a966c6c3c23e46a6f32e0cde0c0b2
Nisha oza <102286625+nishaoza-1432@users.noreply.github.com>
2023-10-26 11:08:21 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package javabasicassignment;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+
+	// TODO Auto-generated method stub
+
+	//Write a Java program to print "Hello, World!" on the screen.
+
+	System.out.println("Hello, World!");
+
+	}
+
+}
